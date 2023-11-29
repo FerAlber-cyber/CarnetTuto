59da32e5d5bb352cbd2b4155967de2404849e4c1
Fernando <feralber202@gmail.com>
2023-11-29 12:01:41 -0600

    Actualizacion

--- app/src/main/java/mx/tecm/ciudadhidalgo/carnetuto/MenuAlumno.kt
+++ app/src/main/java/mx/tecm/ciudadhidalgo/carnetuto/MenuAlumno.kt
@@ -2,10 +2,55 @@ package mx.tecm.ciudadhidalgo.carnetuto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import mx.tecm.ciudadhidalgo.carnetuto.adaptadores.UsuarioAAdapter
+import mx.tecm.ciudadhidalgo.carnetuto.dataClass.UsuarioA
 
 class MenuAlumno : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var usuarioAAdapter: UsuarioAAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_alumno)
+
+        auth = Firebase.auth
+        recyclerView = findViewById(R.id.recyclerViewUsuariosA)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        obtenerDatosUsuarioFirebase()
+    }
+
+    private fun obtenerDatosUsuarioFirebase() {
+        auth.currentUser?.email?.let { email ->
+            val baseDeDatosA = Firebase.firestore
+            baseDeDatosA.collection("usuariosA").document(email).get()
+                .addOnSuccessListener { documento ->
+                    if (documento.exists()) {
+                        val usuarioA = UsuarioA(
+                            "${documento.data?.get("correoInst")}",
+                            "${documento.data?.get("nombreA")}",
+                            "${documento.data?.get("aPaternoA")}",
+                            "${documento.data?.get("aMaternoA")}",
+                            "${documento.data?.get("noControl")}",
+                            "${documento.data?.get("grupoA")}"
+                        )
+                        mostrarDatosEnRecyclerView(usuarioA)
+                    }
+                }
+        }
+    }
+
+    private fun mostrarDatosEnRecyclerView(usuarioA: UsuarioA) {
+        val listaUsuariosA = listOf(usuarioA)
+        usuarioAAdapter = UsuarioAAdapter(listaUsuariosA)
+        recyclerView.adapter = usuarioAAdapter
     }
 }
--- app/src/main/java/mx/tecm/ciudadhidalgo/carnetuto/adaptadores/UsuarioAAdapter.kt
+++ app/src/main/java/mx/tecm/ciudadhidalgo/carnetuto/adaptadores/UsuarioAAdapter.kt
@@ -1,4 +1,42 @@
 package mx.tecm.ciudadhidalgo.carnetuto.adaptadores
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import mx.tecm.ciudadhidalgo.carnetuto.R
+import mx.tecm.ciudadhidalgo.carnetuto.dataClass.UsuarioA
 
-class UsuarioAAdapter {
+class UsuarioAAdapter(private val listaUsuariosA: List<UsuarioA>) :
+    RecyclerView.Adapter<UsuarioAAdapter.UsuarioAViewHolder>() {
+
+    class UsuarioAViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textViewCorreoA: TextView = itemView.findViewById(R.id.textViewCorreoA)
+        val textViewNombreA: TextView = itemView.findViewById(R.id.textViewNombreA)
+        val textViewApellidoPA: TextView = itemView.findViewById(R.id.textViewApellidoPA)
+        val textViewApellidoMA: TextView = itemView.findViewById(R.id.textViewApellidoMA)
+        val textViewNoControl: TextView = itemView.findViewById(R.id.textViewNoControl)
+        val textViewGrupoA: TextView = itemView.findViewById(R.id.textViewGrupoA)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioAViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_usuario_alumno, parent, false)
+        return UsuarioAViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: UsuarioAViewHolder, position: Int) {
+        val usuarioA = listaUsuariosA[position]
+
+        with(holder) {
+            textViewCorreoA.text = "Correo: ${usuarioA.correoInst}"
+            textViewNombreA.text = "Nombre: ${usuarioA.nombreA}"
+            textViewApellidoPA.text = "Apellido Paterno: ${usuarioA.aPaternoA}"
+            textViewApellidoMA.text = "Apellido Materno: ${usuarioA.aMaternoA}"
+            textViewNoControl.text = "No. Control: ${usuarioA.noControl}"
+            textViewGrupoA.text = "Grupo: ${usuarioA.grupoA}"
+        }
+    }
+
+    override fun getItemCount(): Int = listaUsuariosA.size
 }
