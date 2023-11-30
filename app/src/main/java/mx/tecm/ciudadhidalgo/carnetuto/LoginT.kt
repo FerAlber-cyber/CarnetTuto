4cb6cfe0bebb9fc8a6693006321d19eaf276682c
Fernando <feralber202@gmail.com>
2023-11-29 21:37:20 -0600

    Actualizacion

--- app/src/main/java/mx/tecm/ciudadhidalgo/carnetuto/LoginT.kt
+++ app/src/main/java/mx/tecm/ciudadhidalgo/carnetuto/LoginT.kt
@@ -9,7 +9,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import mx.tecm.ciudadhidalgo.carnetuto.dataClass.UsuarioA
 import mx.tecm.ciudadhidalgo.carnetuto.dataClass.UsuarioT
 
 lateinit var usuarioT: UsuarioT
